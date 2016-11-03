9f1a2de9984111b23fefe0576144e88d5c9c740c
Joshua Garcia <joshug4@uci.edu>
2016-11-03 00:36:13 -0700

    updated create_cf_auc_csv.sh to output header row

--- ProjectsData/create_cf_auc_csv.sh
+++ ProjectsData/create_cf_auc_csv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -name "*.cf.out" -exec grep -Hn "AUC" {} \; | sed 's/-AUC:/,/g' | sed 's/\[1\]//g' | gsed -r 's#\./.*(Packages|ARC)/.*\..*\..*\.(.*)\.out:.*: \"(.*)\"#\3,\1-\2#g' | awk '{print toupper($0)}' | sed 's/PACKAGES/PKG/g' | sort --field-separator=',' --key=3
+echo "Models,AUC,Recovery"; find . -name "*.cf.out" -exec grep -Hn "AUC" {} \; | sed 's/-AUC:/,/g' | sed 's/\[1\]//g' | gsed -r 's#\./.*(Packages|ARC)/.*\..*\..*\.(.*)\.out:.*: \"(.*)\"#\3,\1-\2#g' | awk '{print toupper($0)}' | sed 's/PACKAGES/PKG/g' | sort --field-separator=',' --key=3
