67c2ef2253c7c54ec3e7fc92135aa1b0ea5c83c4
Joshua Garcia <joshug4@uci.edu>
2016-11-04 22:16:11 -0700

    - removed pred*.R scripts - created defect count per release scripts and data

---
+++ ProjectsData/defects_per_release.R
@@ -0,0 +1,6 @@
+change<-read.csv("/Users/joshua/ser/projects/arch_prediction/PerRelease/ProjectsData/defects_per_release.csv",header=T)
+p<-ggplot(change, aes(x= Projects, y= Defects, fill= Projects)) + geom_boxplot(width = 0.6) + xlab("") +
+scale_fill_manual(values = c("Cam" = "grey100", "Cas" = "grey90", "Hb" = "grey80", "Hi" = "grey70", "Op" = "grey60"))
+p+guides(fill=FALSE) 
+p+ facet_grid(. ~ Recovery) + theme(legend.position="bottom", text = element_text(size=23), panel.background = element_rect(fill='white', colour = 'black'), axis.text.x = element_text(color = "black"), axis.text.y = element_text(color = "black"), strip.text.x = element_text(family="sans", color = "black"))
+ggsave(file="/Users/joshua/ser/projects/arch_prediction/PerRelease/ProjectsData/defects_per_release.png")
