df44292d99f46b8edd5b2552ed72cc84c8e48e48
Joshua Garcia <joshug4@uci.edu>
2016-11-16 18:39:42 -0800

    added scripts and data for percentage of smell emergence plots

---
+++ Plots/ExtremeChangeBetweenReleases.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+library(plyr)
+options(digits=2)
+
+change<-read.csv("/Users/joshua/ser/projects/arch_prediction/PerRelease/Plots/ExtremeChangeBetweenReleases.csv",header=T)
+p<-ggplot(change, aes(x= Smells, y= Change, fill= Smells)) + geom_boxplot(width = 0.8) + xlab("") + ylab("Change(%)") +
+scale_fill_manual(values = c("SF" = "grey100", "CO" = "grey90", "DC" = "grey80", "LO" = "grey70"))
+p+guides(fill=FALSE) 
+p+ facet_grid(. ~ Recovery) + theme(legend.position="bottom", text = element_text(size=30), panel.background = element_rect(fill='white', colour = 'black'), axis.text.x = element_text(color = "black"), axis.text.y = element_text(color = "black"), strip.text.x = element_text(family="sans", color = "black"))
+ggsave(file="/Users/joshua/ser/projects/arch_prediction/PerRelease/Plots/ExtremeChangeBetweenReleases.png")
