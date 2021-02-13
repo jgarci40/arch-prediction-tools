14e462e9b8819a8038f52b9c729d8ddb813136bc
Joshua Garcia <joshug4@uci.edu>
2021-02-12 21:03:10 -0800

    renaming appendix file

--- ProjectsData/numberOfModules.R
+++ ProjectsData/numberOfModules.R
@@ -1,6 +1,7 @@
-change<-read.csv("/Users/Ehsan/Workspace/RData/Scripts/PerRelease/Plots/multipleReleases/numberOfModules.csv",header=T)
+library(ggplot2)
+change<-read.csv("/Users/joshua/ser_home/projects/arch_prediction/PerRelease/ProjectsData/numberOfModules.csv",header=T)
 p<-ggplot(change, aes(x= Projects, y= Modules, fill= Projects)) + geom_boxplot(width = 0.6) + xlab("") +
 scale_fill_manual(values = c("Cam" = "grey100", "Cas" = "grey90", "Hb" = "grey80", "Hi" = "grey70", "Op" = "grey60"))
 p+guides(fill=FALSE) 
-p+ facet_grid(. ~ Recovery) + theme(legend.position="bottom", text = element_text(size=23), panel.background = element_rect(fill='white', colour = 'black'), axis.text.x = element_text(color = "black"), axis.text.y = element_text(color = "black"), strip.text.x = element_text(family="sans", color = "black"))
-ggsave(file="/Users/Ehsan/Workspace/RData/Scripts/PerRelease/Plots/multipleReleases/numberOfModules.png")
+p+ facet_grid(. ~ Recovery) + theme(legend.position = "none",text = element_text(size=23), panel.background = element_rect(fill='white', colour = 'black'), axis.text.x = element_text(color = "black"), axis.text.y = element_text(color = "black"), strip.text.x = element_text(family="sans", color = "black"))
+ggsave(file="/Users/joshua/ser_home/projects/arch_prediction/PerRelease/ProjectsData/numberOfModules.png")
